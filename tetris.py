59d893090ed908561a80831f659d34eb0cfa751c
Austin Hummel <austin.hummel@gmail.com>
2019-07-31 17:19:12 -0500

    Added piece rotation logic, reworked key press handling

--- tetris.py
+++ tetris.py
@@ -4,9 +4,11 @@ import os
 import sys
 import math
 import time
+import queue
 import random
 import pygame
 import argparse
+import traceback
 import threading
 from pygame.locals import *
 
@@ -19,6 +21,7 @@ class BlockGame:
 		self.y_scalar = int(y_scalar)
 		self.colors = int(colors)
 		self.screen = screen
+		self.running = True
 		self.score = 0
 		self.moves = 0
 		self.counter = 0
@@ -30,8 +33,21 @@ class BlockGame:
 		self.shapes[self.counter] = self.current
 
 	def place_new_shape(self, id, x, y):
-		new_shape = BlockGame.Shape(id, x, y, random.randrange(self.colors), random.randrange(3))
+		new_shape = BlockGame.Shape(id, x, y, color=random.randrange(self.colors), type=random.randrange(3), rotation=0)
+		invalid_pos = False
+		while(not invalid_pos):
+			for block in new_shape.blocks:
+				if block.y < 0:
+					new_shape.update_pos(0, 1)
+				else:
+					invalid_pos = True
+	
 		for block in new_shape.blocks:
+			if self.map[block.x][block.y] != None:
+				self.game_over()
+				print(block.x,block.y)
+				print('game over caused')
+				return
 			self.map[block.x][block.y] = new_shape
 
 		self.shapes[id] = new_shape
@@ -55,24 +71,60 @@ class BlockGame:
 					pygame.draw.rect(screen, (0,0,0),
 					    (self.x_scalar * x, self.y_scalar * y, self.x_scalar ,self.y_scalar))
 
+	def process_event(self, event):
+		if event == 'EXIT' or event == K_ESCAPE:
+			self.running = False
+		if event in (K_LEFT, K_a):
+			self.update_shape_position('left')
+		if event in (K_RIGHT, K_d):
+			self.update_shape_position('right')
+		if event in (K_DOWN, K_s):
+			self.update_shape_position('down')
+		if event in (K_q,):
+			self.rotate_shape('left')
+		if event in (K_e,):
+			self.rotate_shape('right')
+		if event == MOUSEBUTTONDOWN:
+			#game_instance.print_details()
+			self.tick()
+		return True
+	
 	def tick(self):
-		success = self.update_shape_position(None, 'down')
-		##  Unable to move down, if this was triggered by the tick, create a new block
-		if not success:
-			self.counter += 1
-			self.current = self.place_new_shape(self.counter, self.mid, 0)
-			self.shapes[self.counter] = self.current
-			self.clear_full_lines()
-		return
+		self.update_shape_position('down')
 
 	def print_details(self):
 		for id in self.shapes:
 			print(self.shapes[id].id,self.shapes[id].x,self.shapes[id].y)
 	
 	def rotate_shape(self, direction):
-		print(direction)
+		##	could probably optimize this but it's simple enough for the time being
+		shape = self.current
+		if direction == 'right':
+			if shape.rotation == 3:
+				shape.rotation = 0
+			else:
+				shape.rotation += 1
+		if direction == 'left':
+			if shape.rotation == 0:
+				shape.rotation = 3
+			else:
+				shape.rotation -= 1
+		
+		can_move = True
+		for block in shape.blocks:
+			##	Need to implement whatever logic is needed here to handle rotations in tight spaces
+			##	may need to do this before the assignment above, and assign can_move=false
+			pass
+		if can_move == True:
+			for block in shape.blocks:
+				self.map[block.x][block.y] = None
+			shape.update_rotation()
+			self.draw_shape(shape)
+		##	Need to think about how to handle rotating shapes in a close space
+		##	Do I give up if the exact expected rotation doesnt fit
+		##	Or look around for nearby positions that may fit
 
-	def update_shape_position(self, id, dir):
+	def update_shape_position(self, dir, id=None):
 		if dir == 'down':
 			x_dir = 0
 			y_dir = 1
@@ -110,6 +162,13 @@ class BlockGame:
 			self.draw_shape(shape)
 			return True
 		else:
+			##	Only do this on the down dir and only on the current block
+			if dir == 'down' and id == None:
+				##	Was not able to move down anymore.  this should trigger a new shape
+				self.counter += 1
+				self.current = self.place_new_shape(self.counter, self.mid, 0)
+				self.shapes[self.counter] = self.current
+				self.clear_full_lines()
 			return False
 
 	def clear_full_lines(self):
@@ -152,7 +211,7 @@ class BlockGame:
 			##  Move all remaining shapes/blocks down by 1
 			for i in range(lines_cleared):
 				for id in self.shapes:
-					self.update_shape_position(id, 'down')
+					self.update_shape_position('down', id)
 
 		return lines_cleared
 
@@ -169,7 +228,10 @@ class BlockGame:
 		return rows_cleared
 
 	def game_over(self):
-		return False
+		self.running = False
+	
+	def get_state(self):
+		return self.running
 
 	class Block:
 		def __init__(self, shape_id, id, x, y, color):
@@ -180,45 +242,33 @@ class BlockGame:
 			self.color = int(color)
 
 	class Shape:
-		def __init__(self, id, x, y, color, type):
+		def __init__(self, id, x, y, color=color, type=0, rotation=0):
 			self.id = int(id)
 			self.x = int(x)
 			self.y = int(y)
 			self.color = int(color)
 			self.type = int(type)
 			self.blocks = []
-
-			##  Need to create the shape types here
-			if self.type == 0:
-				self.blocks.append(BlockGame.Block(self.id,0,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,1,self.x+1,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,2,self.x,self.y+1,self.color))
-				self.blocks.append(BlockGame.Block(self.id,3,self.x+1,self.y+1,self.color))
-			if self.type == 1:
-				self.blocks.append(BlockGame.Block(self.id,0,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,1,self.x+1,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,2,self.x+2,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,3,self.x+3,self.y,self.color))
-			if self.type == 2:
-				self.blocks.append(BlockGame.Block(self.id,0,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,1,self.x+1,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,2,self.x+1,self.y+1,self.color))
-				self.blocks.append(BlockGame.Block(self.id,3,self.x+2,self.y+1,self.color))
-			if self.type == 3:
-				self.blocks.append(BlockGame.Block(self.id,0,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,1,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,2,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,3,self.x,self.y,self.color))
-			if self.type == 4:
-				self.blocks.append(BlockGame.Block(self.id,0,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,1,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,2,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,3,self.x,self.y,self.color))
-			if self.type == 5:
-				self.blocks.append(BlockGame.Block(self.id,0,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,1,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,2,self.x,self.y,self.color))
-				self.blocks.append(BlockGame.Block(self.id,3,self.x,self.y,self.color))
+			self.rotation = rotation
+			
+			##	Need to define block shapes to avoid confusion
+			##	0, long piece
+			##	1, l piece right
+			##	2, l piece left
+			##	3, square
+			##	4, zag right
+			##	5, zag left
+			##	6, 3 sider
+			long_rotations = [[(-1,0),(0,0),(1,0),(2,0)],[(1,-1),(1,0),(1,1),(1,2)],[(2,-1),(1,-1),(0,-1),(-1,-1)],[(0,-2),(0,-1),(0,0),(0,1)]]
+			l_left_rotations = [[(-1,-1),(-1,0),(0,0),(1,0)],[(1,-1),(0,-1),(0,0),(0,1)],[(1,1),(1,0),(0,0),(-1,0)],[(-1,1),(0,1),(0,0),(0,-1)]]
+			l_right_rotations = [[(-1,0),(0,0),(1,0),(1,-1)],[(0,-1),(0,0),(0,1),(1,1)],[(1,0),(0,0),(-1,0),(-1,1)],[(0,1),(0,0),(0,-1),(-1,-1)]]
+			
+			self.rotations = [long_rotations, l_left_rotations, l_right_rotations]
+			
+			self.blocks.append(BlockGame.Block(self.id,0,self.x + self.rotations[self.type][rotation][0][0],self.y + self.rotations[self.type][rotation][0][1],self.color))
+			self.blocks.append(BlockGame.Block(self.id,1,self.x + self.rotations[self.type][rotation][1][0],self.y + self.rotations[self.type][rotation][1][1],self.color))
+			self.blocks.append(BlockGame.Block(self.id,2,self.x + self.rotations[self.type][rotation][2][0],self.y + self.rotations[self.type][rotation][2][1],self.color))
+			self.blocks.append(BlockGame.Block(self.id,3,self.x + self.rotations[self.type][rotation][3][0],self.y + self.rotations[self.type][rotation][3][1],self.color))
 
 		def update_pos(self, x_diff, y_diff):
 			self.x += x_diff
@@ -226,28 +276,32 @@ class BlockGame:
 			for block in self.blocks:
 				block.x += x_diff
 				block.y += y_diff
+		
+		def update_rotation(self):
+			rotation_values = self.rotations[self.type][self.rotation]
+			for block in self.blocks:
+				block.x = self.x + rotation_values[block.id][0]
+				block.y = self.y + rotation_values[block.id][1]
 
-class screen_draw_thread(threading.Thread):
-	def __init__(self):
-		threading.Thread.__init__(self)
-		self.running = True
-	def run(self):
-		while(running):
-			game_instance.draw_board(screen)
-			pygame.display.flip()
-
+def screen_draw_thread(running):
+	while True:
+		game_instance.draw_board(screen)
+		pygame.display.flip()
+		if not running():
+			break
 
 def main():
 	##	In windows this doesn't work unless there's an x-server running
 	##	Need to start up mobaXterm and the x-server through that to get the window to appear
 	os.environ['SDL_AUDIODRIVER'] = 'dummy'
 	os.environ['SDL_VIDEODRIVER'] = 'x11'
+	os.environ['DISPLAY'] = 'localhost:0.0'
 	pygame.init()
 	random.seed()
 	
 	parser = argparse.ArgumentParser(description='Tetris clone')
-	parser.add_argument('-g', '--grid', type=str, default='8x8', help='size of the gameboard, WxH')
-	parser.add_argument('-c', '--colors', type=int, default=3, help='number of colors to use')
+	parser.add_argument('-g', '--grid', type=str, default='14x16', help='size of the gameboard, WxH')
+	parser.add_argument('-c', '--colors', type=int, default=5, help='number of colors to use')
 	#parser.add_argument('-m', '--moves', type=int, default=20, help='number of moves allowed in a game')
 	parser.add_argument('-s', '--size', type=str, default='640x480', help='size of the game screen')
 
@@ -263,38 +317,27 @@ def main():
 	##	quickly to the drawing thread though
 	global screen, game_instance
 	screen = pygame.display.set_mode((screen_width, screen_height))
+	#pygame.time.Clock.tick(60)
+	pygame.time.set_timer(pygame.USEREVENT, 1000)
 	game_instance = BlockGame(screen, grid_width, grid_height, args.colors, x_scalar, y_scalar)
 
-	global running
 	running = True
-	display_thread = screen_draw_thread()
+	
+	display_thread = threading.Thread(target = screen_draw_thread, args =(lambda : running, ))
+	#display_thread = screen_draw_thread()
 	display_thread.start()
 	try:
 		while(running):
-			if game_instance.game_over():
-				print('Final Score:', int(game_instance.score))
-				running=False
-				break
 			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					running = False
-				elif event.type == KEYDOWN and event.key == K_ESCAPE:
-					running = False
-				elif event.type == KEYDOWN and event.key in (K_LEFT,K_a):
-					game_instance.update_shape_position(None, 'left')
-				elif event.type == KEYDOWN and event.key in (K_RIGHT,K_d):
-					game_instance.update_shape_position(None, 'right')
-				elif event.type == KEYDOWN and event.key in (K_DOWN,K_s):
-					game_instance.update_shape_position(None, 'down')
-				elif event.type == KEYDOWN and event.key in (K_q):
-					game_instance.rotate_piece('left')
-				elif event.type == KEYDOWN and event.key in (K_e):
-					game_instance.rotate_piece('right')
-				elif event.type == MOUSEBUTTONDOWN:
-					#game_instance.print_details()
+				if event.type == USEREVENT:
+					##	move the piece down or create a new one
 					game_instance.tick()
+				if event.type == KEYDOWN:
+					game_instance.process_event(event.key)
+			running = game_instance.get_state()
 	except:
-		print('exception found:',a)
+		traceback.print_exc(file=sys.stdout)
+		running = False
 	finally:
 		display_thread.join()		
 	
